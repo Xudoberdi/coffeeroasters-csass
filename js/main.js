add34e0069dee4a1c4176bde9d78e9c1311d4d87
Xudoberdi <xudoberdimuhammadjonov571@gmail.com>
2022-11-30 08:30:16 +0500

    one part

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const toggleBtn = document.querySelector('.toggele-btn')
 const toggleBtnImg = document.querySelector('.toggele-btn img')
 
 toggleBtn.addEventListener('click', () => {
-  sitenav.classList.toggle('hidden')
+  sitenav.classList.toggle('open')
 
-  if (sitenav.classList.contains('hidden')) {
-    toggleBtnImg.src = '../images/hamburger-icon.svg'
-  } else {
+  if (sitenav.classList.contains('open')) {
     toggleBtnImg.src = '../images/close-icon.svg'
+  } else {
+    toggleBtnImg.src = '../images/hamburger-icon.svg'
   }
 })
